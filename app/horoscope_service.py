329f5ce815a87f9358f46c0875068d77b6287bdd
grizz <grizzlyenot@gmail.com>
2024-12-13 17:33:53 +0300

    Horoscope print realisation added

--- app/horoscope_service.py
+++ app/horoscope_service.py
@@ -1,3 +1,5 @@
+from app.horosopeAPI import HoroscopeAPI
+
 
 ZODIAC_SIGNS = {
     "Aries": 1,
@@ -13,10 +15,27 @@ ZODIAC_SIGNS = {
     "Aquarius": 11,
     "Pisces": 12
 }
-# TODO список знаков по датам на  /start
+
+
+# TODO список знаков по датам на /start
 class HoroscopeService:
-    def __init__(self, zodiac_sign):
-        self.zodiac_sign = get_zodiac_sign()
-    
+    def __init__(self, zodiac_sign: str):
+        self.zodiac_sign = zodiac_sign
+        self.date = None
+
     def get_zodiac_sign(self):
-        return self.zodiac_sign()
+        return self.zodiac_sign
+
+    def get_zodiac_sign_number(self):
+        return ZODIAC_SIGNS.get(self.zodiac_sign)
+
+    def set_date(self, date):
+        self.date = date
+
+    def get_date(self):
+        return self.date
+
+    def get_horoscope(self):
+        greetings = f"Great! That's horoscope for {self.zodiac_sign} for {self.date}"
+        return greetings + HoroscopeAPI.get_horoscope_by_day(self.get_zodiac_sign_number(), self.date)
+
